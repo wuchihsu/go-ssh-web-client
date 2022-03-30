8d66be7af05cff49cd17a9a211791094d6e67a46
wuchihsu <wuchi5457@gmail.com>
2022-03-30 12:27:40 +0800

    use bridgeWSAndSSH func

--- handler.go
+++ handler.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -9,6 +10,7 @@ import (
 	"time"
 
 	"github.com/gorilla/websocket"
+	"golang.org/x/crypto/ssh"
 )
 
 const (
@@ -17,6 +19,10 @@ const (
 
 	// Maximum message size allowed from peer.
 	maxMessageSize = 512
+
+	addr = "127.0.0.1:2222"
+	user = "linuxserver.io"
+	password = "cch"
 )
 
 type windowSize struct {
@@ -34,19 +40,29 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin:     checkOrigin,
 }
 
-func wsWrite(sshCli *sshClient, conn *websocket.Conn) {
-	defer func() {
-		sshCli.Close()
-		conn.Close()
-	}()
-
-	sshCli.SetSessionStderr(os.Stderr)
-	sessOut, err := sshCli.SessionStdoutPipe()
+func getWindowSize(conn *websocket.Conn) (wdSize *windowSize, err error) {
+	conn.SetReadDeadline(time.Now().Add(messageWait))
+	msgType, msg, err := conn.ReadMessage()
+	if msgType != websocket.BinaryMessage {
+		err = fmt.Errorf("conn.ReadMessage: message type is not binary")
+		return
+	}
 	if err != nil {
-		log.Println("wsWrite: sshCli.SessionStdoutPipe:", err)
+		err = fmt.Errorf("conn.ReadMessage: %w", err)
 		return
 	}
 
+	// log.Println("msg:", string(msg))
+
+	wdSize = new(windowSize)
+	if err = json.Unmarshal(msg, wdSize); err != nil {
+		err = fmt.Errorf("json.Unmarshal: %w", err)
+		return
+	}
+	return
+}
+
+func wsWrite(conn *websocket.Conn, sess *ssh.Session, sessOut io.Reader) error {
 	data := make([]byte, maxMessageSize, maxMessageSize)
 
 	for {
@@ -55,111 +71,131 @@ func wsWrite(sshCli *sshClient, conn *websocket.Conn) {
 		if n > 0 {
 			conn.SetWriteDeadline(time.Now().Add(messageWait))
 			if err := conn.WriteMessage(websocket.TextMessage, data[:n]); err != nil {
-				log.Println("wsWrite: conn.WriteMessage:", err)
-				return
+				return fmt.Errorf("conn.WriteMessage: %w", err)
 			}
 		}
 		if readErr != nil {
-			log.Println("wsWrite: sessOut.Read:", readErr)
-			return
+			return fmt.Errorf("sessOut.Read: %w", readErr)
 		}
 	}
 }
 
-func wsRead(sshCli *sshClient, conn *websocket.Conn) {
-	defer func() {
-		sshCli.Close()
-		conn.Close()
-	}()
-
-	sessIn, err := sshCli.SessionStdinPipe()
-	if err != nil {
-		log.Println("wsRead: sshCli.SessionStdinPipe:", err)
-		return
-	}
-
+func wsRead(conn *websocket.Conn, sess *ssh.Session, sessIn io.WriteCloser) error {
 	var zeroTime time.Time
 	conn.SetReadDeadline(zeroTime)
 
 	for {
 		msgType, connReader, err := conn.NextReader()
 		if err != nil {
-			log.Println("wsRead: conn.NextReader:", err)
-			return
+			return fmt.Errorf("conn.NextReader: %w", err)
 		}
 		if msgType != websocket.BinaryMessage {
 			if _, err := io.Copy(sessIn, connReader); err != nil {
-				log.Println("wsRead: io.Copy:", err)
-				return
+				return fmt.Errorf("io.Copy: %w", err)
 			}
 			continue
 		}
+
 		data := make([]byte, maxMessageSize, maxMessageSize)
 		n, err := connReader.Read(data)
 		if err != nil {
-			log.Println("wsRead: connReader.Read:", err)
-			return
+			return fmt.Errorf("connReader.Read: %w", err)
 		}
-		log.Println("data:", string(data))
+
+		// log.Println("data:", string(data))
+
 		var wdSize windowSize
 		if err := json.Unmarshal(data[:n], &wdSize); err != nil {
-			log.Println("wsRead: json.Unmarshal:", err)
-			return
+			return fmt.Errorf("json.Unmarshal: %w", err)
 		}
-		if err := sshCli.PtyWindowChange(wdSize.High, wdSize.Width); err != nil {
-			log.Println("wsRead: sshCli.PtyWindowChange:", err)
-			return
+
+		// log.Println("wdSize:", wdSize)
+
+		if err := sess.WindowChange(wdSize.High, wdSize.Width); err != nil {
+			return fmt.Errorf("sess.WindowChange: %w", err)
 		}
 	}
 }
 
-// sshHandler handles websocket requests for SSH from the clients.
-func sshHandler(w http.ResponseWriter, req *http.Request) {
+func bridgeWSAndSSH(conn *websocket.Conn) {
+	defer conn.Close()
+
+	wdSize, err := getWindowSize(conn)
+	if err != nil {
+		log.Println("bridgeWSAndSSH: getWindowSize:", err)
+		return
+	}
+
+	// log.Println("wdSize:", wdSize)
+
+	// TODO: get addr, user and password from args or a config file
+	config := &ssh.ClientConfig{
+		User: user,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(password),
+		},
+		// InsecureIgnoreHostKey returns a function
+		// that can be used for ClientConfig.HostKeyCallback
+		// to accept any host key.
+		// It should not be used for production code.
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+	client, err := ssh.Dial("tcp", addr, config)
+	if err != nil {
+		log.Println("bridgeWSAndSSH: ssh.Dial:", err)
+		return
+	}
+	defer client.Close()
+
+	session, err := client.NewSession()
+	if err != nil {
+		log.Println("bridgeWSAndSSH: client.NewSession:", err)
+		return
+	}
+	defer session.Close()
+
+	session.Stderr = os.Stderr  // TODO: check proper Stderr output
+	sessOut, err := session.StdoutPipe()
+	if err != nil {
+		log.Println("bridgeWSAndSSH: session.StdoutPipe:", err)
+		return
+	}
+
+	sessIn, err := session.StdinPipe()
+	if err != nil {
+		log.Println("bridgeWSAndSSH: session.StdinPipe:", err)
+		return
+	}
+
+	if err := session.RequestPty("xterm", wdSize.High, wdSize.Width, terminalModes); err != nil {
+		log.Println("bridgeWSAndSSH: session.RequestPty:", err)
+		return
+	}
+	if err := session.Shell(); err != nil {
+		log.Println("bridgeWSAndSSH: session.Shell:", err)
+		return
+	}
+
+	log.Println("started a login shell on the remote host")
+
+	go func() {
+		if err := wsRead(conn, session, sessIn); err != nil {
+			log.Println("bridgeWSAndSSH: wsRead:", err)
+		}
+	}()
+
+	if err := wsWrite(conn, session, sessOut); err != nil {
+		log.Println("bridgeWSAndSSH: wsWrite:", err)
+	}
+}
+
+// handleSSHWebSocket handles websocket requests for SSH from the clients.
+func handleSSHWebSocket(w http.ResponseWriter, req *http.Request) {
 	conn, err := upgrader.Upgrade(w, req, nil)
 	if err != nil {
-		log.Println("sshHandler: upgrader.Upgrade:", err)
+		log.Println("upgrader.Upgrade:", err)
 		return
 	}
 
-	conn.SetReadDeadline(time.Now().Add(messageWait))
-	msgType, msg, err := conn.ReadMessage()
-	if msgType != websocket.BinaryMessage {
-		log.Println("sshHandler: conn.ReadMessage: message type is not binary")
-		conn.Close()
-		return
-	}
-	if err != nil {
-		log.Println("sshHandler: conn.ReadMessage:", err)
-		conn.Close()
-		return
-	}
-
-	log.Println("msg:", string(msg))
-
-	var wdSize windowSize
-	if err := json.Unmarshal(msg, &wdSize); err != nil {
-		log.Println("sshHandler: json.Unmarshal:", err)
-		conn.Close()
-		return
-	}
-
-	sshCli, err := newSSHClient("127.0.0.1:2222", "linuxserver.io", "cch")
-	if err != nil {
-		log.Println("sshHandler: newSSHClient:", err)
-		conn.Close()
-		return
-	}
-
-	go wsWrite(sshCli, conn)
-	go wsRead(sshCli, conn)
-
-	log.Println("wdSize:", wdSize)
-
-	if err := sshCli.PtyAndShell(wdSize.High, wdSize.Width); err != nil {
-		log.Println("sshHandler: sshCli.PtyAndShell:", err)
-		sshCli.Close()
-		conn.Close()
-		return
-	}
-	log.Println("started a login shell on the remote host")
+	go bridgeWSAndSSH(conn)
 }
--- main.go
+++ main.go
@@ -6,9 +6,6 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/web-socket/ssh", sshHandler)
+	http.HandleFunc("/web-socket/ssh", handleSSHWebSocket)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
-// https://i.imgur.com/EOHSWDc.jpg
-// https://i.imgur.com/FqU0AU8.jpg
